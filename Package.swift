7e2accfc81fe5ade21a588e971ed6faa301f1af3
azure-sdk <azuresdk@microsoft.com>
2026-06-16 16:43:04 +0000

    Releasing 1.3.7

---
+++ Package.swift
@@ -0,0 +1,48 @@
+// swift-tools-version:5.3
+//  The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// --------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// The MIT License (MIT)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the ""Software""), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+// --------------------------------------------------------------------------
+
+import PackageDescription
+
+let package = Package(
+    name: "AzureCommunicationCommon",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v12)
+    ],
+    products: [
+        .library(name: "AzureCommunicationCommon", targets: ["AzureCommunicationCommon"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AzureCommunicationCommon",
+            url: "https://github.com/Azure/azure-sdk-for-ios/releases/download/AzureCommunicationCommon_1.3.7/AzureCommunicationCommon_1.3.7.xcframework.zip",
+            checksum: "7045ea78be31c9e52fb5376e30fc05d0255c808bb380d70a41d869d0d93fc016"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
